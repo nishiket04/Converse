186f700cee380e48cfccec7a6eca62d502ae92b9
nishiket04 <nishiket04@gmail.com>
2024-06-04 14:37:02 +0530

    Initial commit

--- app/src/main/java/com/nishiket/converse/MainActivity.java
+++ app/src/main/java/com/nishiket/converse/MainActivity.java
@@ -8,8 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.nishiket.converse.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
