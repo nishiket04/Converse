585921301e52ddb35e4ad5e293024f4965ac1b0f
nishiket04 <nishiket04@gmail.com>
2024-06-06 20:17:04 +0530

    chat model is done

---
+++ app/src/main/java/com/nishiket/converse/model/chat.java
@@ -0,0 +1,31 @@
+package com.nishiket.converse.model;
+
+public class chat {
+    String image;
+    String name;
+    String lastMessage;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+}
