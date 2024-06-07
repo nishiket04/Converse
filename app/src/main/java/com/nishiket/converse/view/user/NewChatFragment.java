4989f35a6237836d99c7c95a3603ab2c7adcebb3
nishiket04 <nishiket04@gmail.com>
2024-06-07 21:31:05 +0530

    add new chat is done

--- app/src/main/java/com/nishiket/converse/view/user/NewChatFragment.java
+++ app/src/main/java/com/nishiket/converse/view/user/NewChatFragment.java
@@ -4,55 +4,41 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.TypefaceSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.nishiket.converse.R;
 import com.nishiket.converse.adapter.ChatAdapter;
-import com.nishiket.converse.databinding.FragmentHomeBinding;
+import com.nishiket.converse.databinding.FragmentNewChatBinding;
 import com.nishiket.converse.model.ChatModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding fragmentHomeBinding;
+public class NewChatFragment extends Fragment {
+    private FragmentNewChatBinding newChatBinding;
     private List<ChatModel> chatModelList = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false);
-        return fragmentHomeBinding.getRoot();
+        newChatBinding = FragmentNewChatBinding.inflate(inflater,container,false);
+        return newChatBinding.getRoot();
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-
         ChatAdapter chatAdapter = new ChatAdapter(getActivity());
-        fragmentHomeBinding.chats.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        fragmentHomeBinding.chats.setAdapter(chatAdapter);
+        newChatBinding.users.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        newChatBinding.users.setAdapter(chatAdapter);
         chatAdapter.setChatModelList(chatModelList);
         chatAdapter.notifyDataSetChanged();
-
-        fragmentHomeBinding.addChat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_newChatFragment);
-            }
-        });
     }
 }
